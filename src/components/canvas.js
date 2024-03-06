d84f94c106f8d64610085d58c0ae81dfbb847549
rencewang <47406212+rencewang@users.noreply.github.com>
2024-03-05 22:59:13 -0800

    new three.js background canvas

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
@@ -8,7 +8,9 @@ import {
   EffectComposer,
   DepthOfField,
   Noise,
+  Vignette,
 } from '@react-three/postprocessing';
+import { KernelSize } from 'postprocessing';
 
 import '../styles/general.scss';
 
@@ -25,13 +27,12 @@ const Controls = () => {
   return (
     <orbitControls
       autoRotate
-      // maxPolarAngle={Math.PI / 3}
-      // minPolarAngle={Math.PI / 3}
-      minDistance={10}
+      minDistance={150}
       maxDistance={150}
       rotateSpeed={0.3}
-      autoRotateSpeed={0.3}
+      autoRotateSpeed={0.05}
       enablePan={false}
+      enableRotate={false}
       args={[camera, gl.domElement]}
       ref={orbitRef} // returning ref to control
     />
@@ -41,87 +42,45 @@ const Controls = () => {
 const Star = () => {
   const [x, y, z] = Array(3)
     .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(300));
-  const size = Math.random() + 1;
+    .map(() => THREE.MathUtils.randFloatSpread(200));
+  const size = Math.random() * 10;
 
   return (
     <mesh position={[x, y, z]}>
       <sphereGeometry args={[size, 25, 25]} />
-      <meshStandardMaterial
-        emissive="#faee1c"
-        emissiveIntensity={0.5}
-        toneMapped={false}
-        // roughness={0.1}
-        metalness={1}
-        // bumpScale={0.005}
-        // clearcoat={1}
-        // clearcoatRoughness={1}
-        // radius={1}
-        // distort={0.4}
-      />
+      <meshStandardMaterial emissive="#010101" emissiveIntensity={1} />
     </mesh>
   );
 };
 
 const ThreeCanvas = () => {
-  // Checking broswer size for three.js zoom
-  const isBrowser = typeof window !== 'undefined';
-  let fov;
-  if (isBrowser) {
-    fov = window.innerWidth > 500 ? 40 : 75;
-  }
-
   const [stars] = useState(
-    Array(300)
+    Array(50)
       .fill()
       .map((key) => <Star key={key} />)
   );
 
   return (
     <Canvas
-      camera={{ position: [0, 0, 90], fov: fov, near: 0.1, far: 1000 }}
-      onCreated={({ gl }) => {
-        gl.shadowMap.enabled = true;
-        gl.shadowMap.type = THREE.PCFSoftShadowMap;
+      camera={{ position: [0, 0, 0], fov: 50 }}
+      gl={{
+        powerPreference: 'high-performance',
       }}
     >
-      <ambientLight intensity={0.8} />
-      <spotLight position={[0, 200, 0]} intensity={1} penumbra={1} castShadow />
+      <fog color="#ccc" attach="fog" near={100} far={300} />
       <Controls />
-
-      <mesh rotation={[20, 10, 50]}>
-        <sphereGeometry args={[15, 100, 100]} />
-        <meshStandardMaterial
-          // color={'#000000'}
-          // roughness={0.1}
-          metalness={1}
-          // bumpScale={0.005}
-          // clearcoat={1}
-          // clearcoatRoughness={1}
-          // radius={1}
-          // distort={0.4}
-          emissive="#fff"
-          emissiveIntensity={0.5}
-          toneMapped={false}
-          wireframe={true}
-        />
-      </mesh>
-
       <EffectComposer multisampling={0} disableNormalPass={true}>
-        <DepthOfField
-          focusDistance={0}
-          focalLength={0.02}
-          bokehScale={2}
-          height={1000}
-        />
+        <DepthOfField focusDistance={10} focalLength={0} height={10} />
         <Bloom
-          luminanceThreshold={0}
-          luminanceSmoothing={0.9}
-          height={300}
-          opacity={3}
+          kernelSize={KernelSize.LARGE}
+          luminanceThreshold={1}
+          luminanceSmoothing={1}
+          mipmapBlur={false}
+          height={10}
+          opacity={100}
         />
-
-        <Noise opacity={0.025} />
+        <Noise opacity={0.1} />
+        <Vignette eskil={false} offset={0.1} darkness={0.4} />
       </EffectComposer>
 
       {stars}
