0efbdaeb2d0d45f3d365faa16cf0ddd8d1fc6c00
rencewang <47406212+rencewang@users.noreply.github.com>
2024-03-07 01:26:00 -0800

    add category filtering to projects

--- src/content/projects/projects.js
+++ src/content/projects/projects.js
@@ -1,44 +1,50 @@
 const projects = [
   {
-    title: 'Wall',
-    year: '2019-Present',
-    description:
-      'Type-foundry-esque platform for logging daily thoughts, monthly playlists, inspirational quotes, and spontaneous audio. Powered by AWS Amplify.',
-    link: 'https://wall.rence.la/',
-  },
-  {
     title: 'Spotlist Analysis',
     year: '2022',
     description:
       'Expansion on Spotlist that offers insights into user created playlists, such as genre, history, and audio features. Built with Next.js. [Under Development].',
+    category: 'Tool',
     link: 'https://spot.rence.la/',
   },
   {
-    title: 'Remember',
-    year: '2022',
+    title: 'Wall',
+    year: '2019-2022',
     description:
-      "Things that we should remember even as we get older. Hopefully it'll provide courage for you to fight another day. [Under Development].",
-    link: 'https://remember.rence.la/',
+      'Type-foundry-esque platform for logging daily thoughts, monthly playlists, inspirational quotes, and spontaneous audio. Powered by AWS Amplify. [Under Development].',
+    category: 'Tool',
+    link: 'https://wall.rence.la/',
   },
   {
     title: "Media's Role in American Public Opinion of China",
     year: '2022',
     description:
       'Final project for PLSC 247: Media and Democracy, Fall 2022 Yale College. Research on how coverage of several key issues about China led US public opinion.',
+    category: 'Research',
     link: 'https://media.rence.la/',
+  },
+  {
+    title: 'Remember',
+    year: '2022',
+    description:
+      "Things that we should remember even as we get older. Hope it'll provide courage to fight another day.",
+    category: 'Creative',
+    link: 'https://remember.rence.la/',
   },
   {
     title: 'Electoral System in the P.R.C.',
     year: '2022',
     description:
       'Project for PLSC 358: Comparative Political Parties and Electoral Systems, Fall 2022 Yale College. Overview of how the electoral system in the P.R.C. is supposed to work, at least on paper.',
+    category: 'Research',
     link: 'https://prc.rence.la/',
   },
   {
     title: 'Chinese Idioms',
     year: '2022',
     description:
-      'Presenting some of my favorite Chinese idioms with infinite scroll, SVG stretch animation, and a dictionary. [Under Development].',
+      'Presenting some of my favorite Chinese idioms with infinite scroll, SVG stretch animation, and a dictionary.',
+    category: 'Creative',
     link: 'https://idioms.rence.la/',
   },
   {
@@ -46,12 +52,14 @@ const projects = [
     year: '2020-2021',
     description:
       'Retrieve information about any Spotify playlist because Spotify makes it really hard to copy song names and I cannot type Korean. Built with React and Spotify API',
+    category: 'Tool',
     link: 'https://spotlist.netlify.app/',
   },
   {
     title: 'Planned Universe',
     year: '2020',
     description: 'Blog for collecting floor plans. Built with Gatsby',
+    category: 'Tool',
     link: 'https://universe.rence.la/',
   },
   {
@@ -59,6 +67,7 @@ const projects = [
     year: '2019',
     description:
       'An ode to old age. Interactive poetry project that is actually a bunch of song lyrics pieced together.',
+    category: 'Creative',
     link: 'https://inmydefense.rence.la/',
   },
   {
@@ -66,12 +75,14 @@ const projects = [
     year: '2019',
     description:
       'Showcase of book-binding project for ART 006: Art of the Printed Word, Fall 2019 Yale College. Brought online using the Last.fm API',
+    category: 'Creative',
     link: 'https://10of10s.netlify.app/',
   },
   {
     title: 'Breathe',
     year: '2019',
     description: 'Minimalist new page extension that reminds you to breathe.',
+    category: 'Tool',
     link: 'https://chrome.google.com/webstore/detail/breathe-minimalist-new-ta/ngcbnnpkcfomilobdpffbkcabmehjnbm',
   },
   {
@@ -79,6 +90,7 @@ const projects = [
     year: '2019',
     description:
       "Minisite for the Yale Politic's special coverage of some 2020 election candidates.",
+    category: 'Creative',
     link: 'https://chiaski.github.io/politic-hopefuls/',
   },
 ];
--- src/pages/project.js
+++ src/pages/project.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Seo from '../components/seo';
 import projects from '../content/projects/projects';
 import '../styles/general.scss';
 
 const ProjectPage = () => {
+  const [displayedProjects, setDisplayedProjects] = useState(projects);
+  const filterProjects = (category) => {
+    if (category === 'All') {
+      return projects;
+    }
+    return projects.filter((project) => project.category === category);
+  };
+
   return (
     <>
       <Seo title="Projects" />
@@ -12,20 +20,52 @@ const ProjectPage = () => {
       <section id="project">
         <div className="page-filter">
           <div className="link-button" aria-hidden="true">
-            <span className="highlight">All</span>
+            <span
+              className="highlight"
+              role="presentation"
+              onClick={() => {
+                setDisplayedProjects(filterProjects('All'));
+              }}
+            >
+              All
+            </span>
           </div>
           <div className="link-button" aria-hidden="true">
-            <span className="highlight">Tools</span>
+            <span
+              className="highlight"
+              role="presentation"
+              onClick={() => {
+                setDisplayedProjects(filterProjects('Tool'));
+              }}
+            >
+              Tools
+            </span>
           </div>
           <div className="link-button" aria-hidden="true">
-            <span className="highlight">Nothings</span>
+            <span
+              className="highlight"
+              role="presentation"
+              onClick={() => {
+                setDisplayedProjects(filterProjects('Creative'));
+              }}
+            >
+              Creative
+            </span>
           </div>
           <div className="link-button" aria-hidden="true">
-            <span className="highlight">Research</span>
+            <span
+              className="highlight"
+              role="presentation"
+              onClick={() => {
+                setDisplayedProjects(filterProjects('Research'));
+              }}
+            >
+              Research
+            </span>
           </div>
         </div>
 
-        {projects.map((project, index) => (
+        {displayedProjects.map((project, index) => (
           <details key={index} open={index < 3}>
             <summary>
               <span className="title highlight">{project.title}</span>
@@ -34,8 +74,8 @@ const ProjectPage = () => {
             </summary>
 
             <span className="highlight">{project.description}</span>
-            <a href={project.link}>
-              <span className="highlight">View</span>
+            <a href={project.link} target="_blank" rel="noreferrer">
+              <span className="highlight">View &#x2197;</span>
             </a>
           </details>
         ))}
