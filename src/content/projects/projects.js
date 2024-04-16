ef7680d5ee4d5dd32cd5e0688bf3a00c66888f1c
rencewang <47406212+rencewang@users.noreply.github.com>
2024-04-16 00:16:07 -0700

    modify projects with new links

--- src/content/projects/projects.js
+++ src/content/projects/projects.js
@@ -1,19 +1,27 @@
 const projects = [
   {
+    title: 'Wall (on Rails)',
+    year: '2024',
+    description:
+      'Microblog powered by Ruby on Rails, as another iteration of Wall. Allows user registration, content management, and discovery. [Under Development].',
+    category: 'Tool',
+    link: 'https://wall.rence.la/',
+  },
+  {
     title: 'Spotlist Analysis',
     year: '2022',
     description:
-      'Expansion on Spotlist that offers insights into user created playlists, such as genre, history, and audio features. Built with Next.js. [Under Development].',
+      'Expansion on Spotlist that offers insights into user created playlists, such as genre, history, and audio features. Built with Next.js.',
     category: 'Tool',
     link: 'https://spot.rence.la/',
   },
   {
-    title: 'Wall',
+    title: 'Wall (2022)',
     year: '2019-2022',
     description:
-      'Type-foundry-esque platform for logging daily thoughts, monthly playlists, inspirational quotes, and spontaneous audio. Powered by AWS Amplify. [Under Development].',
-    category: 'Tool',
-    link: 'https://wall.rence.la/',
+      'Type-foundry-esque platform for logging daily thoughts, monthly playlists, inspirational quotes, and spontaneous audio. Powered by AWS Amplify.',
+    category: 'Creative',
+    link: 'https://rencewang.github.io/wall-2022',
   },
   {
     title: "Media's Role in American Public Opinion of China",
@@ -21,7 +29,7 @@ const projects = [
     description:
       'Final project for PLSC 247: Media and Democracy, Fall 2022 Yale College. Research on how coverage of several key issues about China led US public opinion.',
     category: 'Research',
-    link: 'https://media.rence.la/',
+    link: 'https://rencewang.github.io/PLSC-247',
   },
   {
     title: 'Remember',
@@ -29,7 +37,7 @@ const projects = [
     description:
       "Things that we should remember even as we get older. Hope it'll provide courage to fight another day.",
     category: 'Creative',
-    link: 'https://remember.rence.la/',
+    link: 'https://rencewang.github.io/remember',
   },
   {
     title: 'Electoral System in the P.R.C.',
@@ -37,7 +45,7 @@ const projects = [
     description:
       'Project for PLSC 358: Comparative Political Parties and Electoral Systems, Fall 2022 Yale College. Overview of how the electoral system in the P.R.C. is supposed to work, at least on paper.',
     category: 'Research',
-    link: 'https://prc.rence.la/',
+    link: 'https://rencewang.github.io/PLSC-358',
   },
   {
     title: 'Chinese Idioms',
@@ -45,7 +53,7 @@ const projects = [
     description:
       'Presenting some of my favorite Chinese idioms with infinite scroll, SVG stretch animation, and a dictionary.',
     category: 'Creative',
-    link: 'https://idioms.rence.la/',
+    link: 'https://rencewang.github.io/idioms',
   },
   {
     title: 'Spotlist',
@@ -68,7 +76,7 @@ const projects = [
     description:
       'An ode to old age. Interactive poetry project of a bunch of song lyrics pieced together.',
     category: 'Creative',
-    link: 'https://inmydefense.rence.la/',
+    link: 'https://rencewang.github.io/inmydefense',
   },
   {
     title: '10 Albums of 2010s',
